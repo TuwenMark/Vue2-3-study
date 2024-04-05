d91ba3e2fc0598f49dc8dc3e72687a8f579626c0
MarkTuwen <m18698577021@163.com>
2024-04-05 21:12:54 +0800

    组件化-小兔鲜购物网站

---
+++ src/components/BaseGoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <a href="#">
+      <div class="pic">
+        <img src="@/assets/images/goods1.png" alt="" />
+      </div>
+      <div class="txt">
+        <h4>KN95级莫兰迪色防护口罩</h4>
+        <p>¥ <span>79</span></p>
+      </div>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
